8c5a476f3ccfbae6c8dbf5ad83b57eed539f940c
Yotam Olenik <yotamolenik@gmail.com>
2022-10-18 16:38:20 +0300

    payload types: extend functionality to work with oop

--- mobileconfig/__main__.py
+++ mobileconfig/__main__.py
@@ -2,9 +2,12 @@ import csv
 import plistlib
 from pathlib import Path
 from typing import Mapping
+import mdutils
 
 import click
-from humanfriendly.tables import format_smart_table
+
+# im not yet sure why, but pycharm thinks this import is wrong. in fact it works perfectly fine on my terminal
+from mobileconfig import MobileConfig
 
 XML_HEADER = b'<?xml '
 XML_TAIL = b'</plist>'
@@ -37,63 +40,60 @@ def consents(ctx):
 
 
 @cli.command()
-@click.argument('csv_summary', type=click.File('w'), required=False)
+@click.argument('output_file', type=click.File('w'), required=False)
+# @click.option('--output_type', type=click.Choice(['csv', 'md']))
 @click.pass_context
-def payload_types(ctx, csv_summary):
-    """ Print all PayloadType's (with an optional .csv output) """
-    if csv_summary:
-        csv_summary = csv.writer(csv_summary)
+def payload_types(ctx, output_file):
+    """ Print all PayloadTypes to either a csv or md file """
+    if output_file:
+        output_file = csv.writer(output_file)
 
     header = ['Display Name', 'PayloadType', 'Description']
 
-    if csv_summary:
-        csv_summary.writerow(header)
+    if output_file:
+        output_file.writerow(header)
+    # if output_type == 'md':
+    #     mdfile = mdutils.MdUtils(file_name='Example_Markdown', title='Markdown File Example')
 
     rows = []
     for plist in ctx.obj['plists']:
-        display_name = plist['PayloadDisplayName']
-        for payload_content in plist['PayloadContent']:
-            payload_type = payload_content['PayloadType']
-
-            description = ''
-            if payload_type == 'com.apple.system.logging':
-                subsystems = payload_content.get('Subsystems')
-                if subsystems:
-                    description = 'Subsystems: '
-                    description += ', '.join(subsystems.keys())
-            elif payload_type == 'com.apple.defaults.managed':
-                for inner_payload_content in payload_content['PayloadContent']:
-                    domain = inner_payload_content.get('DefaultsDomainName')
-                    data_keys = ', '.join(inner_payload_content.get('DefaultsData').keys())
-                    description = f'{domain}: {data_keys}'
-
+        mobileconfig = MobileConfig(plist)
+        display_name = mobileconfig.payload_display_name
+        print('display name is', display_name)
+        for payload_content in mobileconfig.payload_content:
+            print('payload content is ', payload_content)
+            payload_type = payload_content.payload_type
+            description = str(payload_content)
             rows.append((display_name, payload_type, description))
 
-    if csv_summary:
-        csv_summary.writerows(rows)
-
-    print(format_smart_table(rows, header))
+    if output_file:
+        output_file.writerows(rows)
+    # if output_type == 'md':
+    #     mdfile.create_md_file()
 
 
 @cli.command()
 @click.pass_context
 def gist(ctx):
-    """ print device configurations in input dir for all profiles """
+    """ create a markdown file containing device configurations in input dir for all profiles """
+    mdfile = mdutils.MdUtils(file_name='profiles', title='profiles list')
     for plist in ctx.obj['plists']:
         display_name = plist['PayloadDisplayName']
-        print(display_name+':')
+        mdfile.new_header(level=1, title='Profile Name: ' + display_name)
+        mdfile.new_header(level=2, title='payload types:')
         for payload_content in plist['PayloadContent']:
             payload_type = payload_content['PayloadType']
-            print('\t' + payload_type)
+            mdfile.new_header(level=3, title='payload type: ' + payload_type)
             if payload_type == 'com.apple.system.logging':
                 subsystems = payload_content.get('Subsystems')
                 if subsystems:
                     for subsystem in payload_content['Subsystems']:
-                        print('\t\t'+subsystem)
+                        mdfile.new_header(level=4, title='subsystem: ' + subsystem)
             elif payload_type == 'com.apple.defaults.managed':
                 for inner_payload_content in payload_content['PayloadContent']:
-                    print('\t' + inner_payload_content['DefaultsDomainName'])
-                    print('\t\t' + str(inner_payload_content['DefaultsData']))
+                    mdfile.new_header(level=3, title='Domain name: ' + inner_payload_content['DefaultsDomainName'])
+                    mdfile.new_header(level=4, title='inner payload values: ' + str(inner_payload_content['DefaultsData']))
+    mdfile.create_md_file()
 
 
 @cli.command()
---
+++ mobileconfig/mobileconfig.py
@@ -0,0 +1,73 @@
+from typing import Mapping, List
+
+
+class PayloadContent:
+    @staticmethod
+    def create(plist: Mapping) -> 'PayloadContent':
+        return {
+            'com.apple.system.logging': LoggingPayload,
+            'com.apple.defaults.managed': ManagedPayload,
+            'com.apple.corecapture.configure': CoreCapture,
+            'com.apple.ManagedClient.preferences': ManagedClient
+        }[plist['PayloadType']](plist)
+
+    def __init__(self, plist: Mapping):
+        self._plist = plist
+
+    @property
+    def payload_content(self):
+        return self._plist['PayloadContent']
+
+    @property
+    def core_capture_config(self):
+        """ a special property present only in payloads of type com.apple.ManagedClient.preferences """
+        return self._plist['CoreCaptureConfig']
+
+    @property
+    def payload_type(self):
+        return self._plist['PayloadType']
+
+
+class LoggingPayload(PayloadContent):
+
+    def __repr__(self):
+        subsystems = self._plist.get('Subsystems')
+        description = ''
+        if subsystems:
+            description = 'Subsystems: '
+            description += ', '.join(subsystems.keys())
+        return description
+
+
+class ManagedPayload(PayloadContent):
+
+    def __repr__(self):
+        inner_payload_content = self.payload_content
+        domain = inner_payload_content[0].get('DefaultsDomainName')
+        data_keys = ', '.join(inner_payload_content[0].get('DefaultsData').keys())
+        description = f'{domain}: {data_keys}'
+        return description
+
+
+class CoreCapture(PayloadContent):
+
+    def __repr__(self):
+        return str(self.core_capture_config)
+
+
+class ManagedClient(PayloadContent):
+    def __repr__(self):
+        return str(self.payload_content)
+
+
+class MobileConfig:
+    def __init__(self, plist: Mapping):
+        self._plist = plist
+
+    @property
+    def payload_display_name(self) -> str:
+        return self._plist['PayloadDisplayName']
+
+    @property
+    def payload_content(self) -> List[PayloadContent]:
+        return [PayloadContent.create(p) for p in self._plist['PayloadContent']]
